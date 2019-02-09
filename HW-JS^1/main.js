b97b9b0b5e0b7b2a8772e2d58e510c5de05f9792
morgan2009 <46084543+morgan2009@users.noreply.github.com>
2019-02-09 10:56:12 +0200

    Add files via upload

---
+++ HW-JS^1/main.js
@@ -0,0 +1,48 @@
+/*2*/
+var myString = 'Lorem';
+var myNumber = 25;
+var myBool = true;
+var array = [1,2,3];
+var obj = {name: 'Alan'};
+console.log(myString,myNumber,myBool,array,obj)
+/*3*/
+var i = 10;
+console.log(i.toFixed(2))
+/*4*/
+var i = 10;
+console.log(++i);
+console.log(i);
+console.log(i++);
+console.log(i)
+/*5*/
+var i = 10;
+i+= 5;
+console.log(i);
+i-= 3;
+console.log(i);
+i*= 10;
+console.log(i);
+i/= 5;
+console.log(i);
+i%= 50;
+console.log(i)
+/*6*/
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random(1,10));
+console.log(Math.pow(2,5))
+/*7*/
+var myString = 'Lorem lorem lorem';
+console.log(myString.length)
+/*8*/
+var myString = 'Lorem ipsum dolor sit amet';
+console.log(myString.indexOf('sit'));
+console.log(myString.indexOf('bla'))
+
+/*9*/
+var myString = 'Lorem ipsum dolor sit';
+console.log(myString.replace('dolor', 'frame'))
+/*10*/
+var myString = 'ffffffffffAZffffff';
+console.log(myString.toUpperCase('ffffffffffAZffffff'));
+console.log(myString.toLowerCase('ffffffffffAZffffff'))
